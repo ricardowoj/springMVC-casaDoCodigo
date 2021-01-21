3063037c7698eb6ee80c3464f49e91bb3aca9b69
ricardowoj <ricardowoj@hotmail.com>
2021-01-21 08:45:46 -0300

    Refatorando imports ProductsController

--- CasaDoCodigo/src/main/java/br/com/casadocodigo/loja/controllers/ProductsController.java
+++ CasaDoCodigo/src/main/java/br/com/casadocodigo/loja/controllers/ProductsController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import br.com.casadocodigo.loja.daos.ProductDAO;
 import br.com.casadocodigo.loja.models.Product;
 
 @Controller
