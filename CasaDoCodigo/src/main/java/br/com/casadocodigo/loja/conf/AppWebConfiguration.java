06a5772a9e53994e7890dd37be08854151df83bb
ricardowoj <ricardowoj@hotmail.com>
2021-01-20 22:13:24 -0300

    Configuração básica, acesando o primeiro endereço e habilitando o Spring MVC.

---
+++ CasaDoCodigo/src/main/java/br/com/casadocodigo/loja/conf/AppWebConfiguration.java
@@ -0,0 +1,36 @@
+package br.com.casadocodigo.loja.conf;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+import br.com.casadocodigo.loja.controllers.HomeController;
+
+@EnableWebMvc
+@ComponentScan(basePackageClasses = { HomeController.class })
+public class AppWebConfiguration {
+	/*
+	 * O objetivo principal dela é expor para a Servlet do Spring MVC quais são as
+	 * classes que devem ser lidas e carregadas.
+	 * 
+	 * @ComponentScan. Através dela indicamos quais pacotes devem ser lidos.
+	 * EnableWebMvc habilita várias funcionalidades: • Conversão de objetos para XML
+	 * • Conversão de objetos para JSON • Validação usando a especificação • Suporte
+	 * a geração de RSS
+	 */
+	@Bean
+	public InternalResourceViewResolver internalResorceViewResolver() {
+		/*
+		 * InternalResourceViewResolver, como pode ser lido no próprio código, guarda as
+		 * configurações da pasta base e do sufixo que devem ser adicionados para
+		 * qualquer caminho que seja retornado por métodos dos controllers. A
+		 * annotation @Bean indica para o Spring que o retorno desse método deve ser
+		 * registrado como um objeto gerenciado pelo container.
+		 */
+		InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+		resolver.setPrefix("/WEB-INF/views/");
+		resolver.setSuffix(".jsp");
+		return resolver;
+	}
+}
---
+++ CasaDoCodigo/src/main/java/br/com/casadocodigo/loja/conf/ServletSpringMVC.java
@@ -0,0 +1,25 @@
+package br.com.casadocodigo.loja.conf;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class ServletSpringMVC extends AbstractAnnotationConfigDispatcherServletInitializer {
+	//O objetivo de herdar dessa classe é justamente não ter que ficar fazendo
+	//o registro do DispatcherServlet na mão.
+	
+	@Override
+	protected Class<?>[] getRootConfigClasses() {
+		return null;
+	}
+	
+	@Override
+	protected Class<?>[] getServletConfigClasses() {
+		return new Class[] {AppWebConfiguration.class};
+	}
+	
+	@Override
+	protected String[] getServletMappings() {
+		//Aqui é onde você diz qual é o padrão de endereço que vai ser delegado 
+		//para o Servlet do Spring MVC.
+		return new String[] {"/"};
+	}
+}
