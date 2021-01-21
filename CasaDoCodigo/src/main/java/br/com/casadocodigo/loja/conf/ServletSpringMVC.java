1fceaf69e6e9a451e7d6d5aa9df882ff65684ab7
ricardowoj <ricardowoj@hotmail.com>
2021-01-21 08:46:41 -0300

    Configuração programática

--- CasaDoCodigo/src/main/java/br/com/casadocodigo/loja/conf/ServletSpringMVC.java
+++ CasaDoCodigo/src/main/java/br/com/casadocodigo/loja/conf/ServletSpringMVC.java
@@ -3,23 +3,23 @@ package br.com.casadocodigo.loja.conf;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
 public class ServletSpringMVC extends AbstractAnnotationConfigDispatcherServletInitializer {
-	//O objetivo de herdar dessa classe é justamente não ter que ficar fazendo
-	//o registro do DispatcherServlet na mão.
-	
+	// O objetivo de herdar dessa classe é justamente não ter que ficar fazendo
+	// o registro do DispatcherServlet na mão.
+
 	@Override
 	protected Class<?>[] getRootConfigClasses() {
 		return null;
 	}
-	
+
 	@Override
 	protected Class<?>[] getServletConfigClasses() {
-		return new Class[] {AppWebConfiguration.class};
+		return new Class[] { AppWebConfiguration.class, JPAConfiguration.class };
 	}
-	
+
 	@Override
 	protected String[] getServletMappings() {
-		//Aqui é onde você diz qual é o padrão de endereço que vai ser delegado 
-		//para o Servlet do Spring MVC.
-		return new String[] {"/"};
+		// Aqui é onde você diz qual é o padrão de endereço que vai ser delegado
+		// para o Servlet do Spring MVC.
+		return new String[] { "/" };
 	}
 }
