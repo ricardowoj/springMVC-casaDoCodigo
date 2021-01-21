bf7918db9ce7934527d658d62a012f3b63ce6427
ricardowoj <ricardowoj@hotmail.com>
2021-01-21 08:32:30 -0300

    URL para chegar ao formulário

---
+++ CasaDoCodigo/src/main/java/br/com/casadocodigo/loja/controllers/ProductsController.java
@@ -0,0 +1,26 @@
+package br.com.casadocodigo.loja.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import br.com.casadocodigo.loja.models.Product;
+
+@Controller
+public class ProductsController {
+	
+	@Autowired
+	private ProductDAO productDAO = new ProductDAO();
+	
+	@RequestMapping("/produtos")
+	public String save(Product product) {
+		System.out.println("Cadastrando produto " + product);
+		productDAO.save(product);
+		return "products/ok";
+	}
+	
+	@RequestMapping("/produtos/form")
+	public String form() {
+		return "products/form";
+	}
+}
